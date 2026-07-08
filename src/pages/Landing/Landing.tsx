8ce4d0abc949c09950e338bd26c24bfd25584200
srushti981 <srushtihiremath0987@gmail.com>
2026-07-08 23:02:44 +0530

    Fix Layout import casing

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,8 +1,8 @@
-import Navbar from "../../components/layout/Navbar";
+import Navbar from "../../components/Layout/Navbar";
 import Hero from "../../components/Landing/Hero";
 import Stats from "../../components/Landing/Stats";
 import Features from "../../components/Landing/Features";
-import Footer from "../../components/layout/Footer";
+import Footer from "../../components/Layout/Footer";
 import Testimonials from "../../components/Landing/Testimonials";
 import CTA from "../../components/Landing/CTA";
 
